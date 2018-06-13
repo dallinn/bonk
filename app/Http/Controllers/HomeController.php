6dea0836afd69b201d7508e48c0df615d0d77e24
dallinn <dallinnickle@gmail.com>
2018-06-13 14:40:14 -0600

    order post by creation date descending

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
 
     public function testHome()
     {
-        $data['posts'] = Post::all();
+        $data['posts'] = Post::orderBy('created_at', 'desc')->get();
         return view('home', $data);
     }
 
